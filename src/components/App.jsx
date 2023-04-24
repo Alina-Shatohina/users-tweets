704c38b8955cd01693e6175bc58caa99421ad853
Andrew-Shatokhin <114595292+Alina-Shatohina@users.noreply.github.com>
2023-04-25 01:08:24 +0300

    rev 11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,17 @@ import  SharedLayout from './SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Tweets = lazy(() => import('../pages/Tweets/Tweets'));
-
+const NotFound = lazy(() => import('../pages/NotFound'));
 
 export const App = () => {
   return (
   <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/tweets" element={<Tweets/>}/>
+        <Route path="/tweets" element={<Tweets />} />
       </Route>
-
+      
+      <Route path="*" element={<NotFound />} />
   </Routes>
   );
 };
